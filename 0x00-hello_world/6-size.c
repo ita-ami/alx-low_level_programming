43fbf502abd9dfcd064ba401e8ccc17c154b9d75
ita-ami <belkerfjaouad@gmail.com>
2023-06-15 15:20:00 -0700

    add tsks files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-
+{
 	printf("Size of a char: %d\n",sizeof(char));
 	printf("Size of an int: %d\n",sizeof(int));
 	printf("Size of a long int: %d\n",sizeof(long int));
