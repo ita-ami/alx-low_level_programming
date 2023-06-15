303ecd3a779dc37168455a55a88ec35e20b33985
ita-ami <belkerfjaouad@gmail.com>
2023-06-15 09:53:54 -0700

    add files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-/* entry point */
+/**
+ * main - Entry point
+ * 
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ * 
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char jaw[] = "\"Programming is like building a multilingual puzzle";
