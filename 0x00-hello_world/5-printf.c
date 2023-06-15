b26ea38e7236add8eef0f77de09ea24daec6cc0e
ita-ami <belkerfjaouad@gmail.com>
2023-06-15 10:07:30 -0700

    add files

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 /**
- * this is a entry point
+ * main - Entry point
+ * 
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ * 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
