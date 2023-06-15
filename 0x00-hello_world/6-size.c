d5adb843004e24103d21dffaf2f70dfdf6c3f201
ita-ami <belkerfjaouad@gmail.com>
2023-06-15 15:18:20 -0700

    add tsks files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,11 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	printf("Size of a char: %d\n",sizeof(char));
+	printf("Size of an int: %d\n",sizeof(int));
+	printf("Size of a long int: %d\n",sizeof(long int));
+	printf("Size of a long long int: %d\n",sizeof(long long int));
+	printf("Size of a float: %d\n",sizeof(float));
 	return (0);
 }
