a80683b506b16f5f55752675ee812528846c325b
ita-ami <belkerfjaouad@gmail.com>
2023-06-15 09:22:51 -0700

    add new task files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-int main(void)
+int main(void);
+
 {
-    char jaw[] = "\"Programming is like building a multilingual puzzle";	   
-    puts(jaw);
-    return 0 ;
+	char jaw[] = "\"Programming is like building a multilingual puzzle";
+	puts(jaw);
+	return 0 ;
 }
