eb394e5e7afe7d7969ddcd435235225e8e796a1e
ita-ami <belkerfjaouad@gmail.com>
2023-06-16 16:36:25 -0700

    add file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,11 +7,11 @@ int main(void)
 
      scanf("%d",&n);
 
-     if (n>0)
+     if ( n > 0 )
 {
                 printf("%d is positive\n",n);
 }
-     else if (n==0)
+     else if ( n == 0 )
 { 
                 printf("%d is zero\n",n);
 }
