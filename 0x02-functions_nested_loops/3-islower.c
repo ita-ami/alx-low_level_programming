0fa4cf2e5f4e4ed78aa753d22da2fecd66b5585a
ita-ami <belkerfjaouad@gmail.com>
2023-06-20 16:54:01 -0700

    3. islower mandatory Write a function that che

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _islower - function that checks for lowercase character
+*/
+int _islower(int c)
+{
+int c ;
+
+if ((c >= 'a') && (c <= 'a'))
+{
+return (1);
+}
+else
+return (0);
+}
