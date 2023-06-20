bb1d2883956df687c378644e7fbf7df11f5b06ff
ita-ami <belkerfjaouad@gmail.com>
2023-06-20 16:25:49 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* print_alphabet_x1 - function that print the lowercase alphabet 10 times
+* print_alphabet_x10 - function that print the lowercase alphabet 10 times
 */
 void print_alphabet_x10(void)
 {
