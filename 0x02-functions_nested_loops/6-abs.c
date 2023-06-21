6b83c668986e25ac43ca136062e8967c24e6555c
ita-ami <belkerfjaouad@gmail.com>
2023-06-21 05:33:05 -0700

    update

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
 *_abs - a  function that computes the absolute value of an integer.
+*Return: always 0 (success)
 */
 int _abs(int)
 {
@@ -17,4 +18,5 @@ int m;
 m = n * (-1);
 _putchar(m);
 }
+return (0);
 }
