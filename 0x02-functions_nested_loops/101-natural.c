d8dc6f34f3926ce85c3b604b0767ce2d41a98958
ita-ami <belkerfjaouad@gmail.com>
2023-06-21 16:23:14 -0700

    update

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,33 +3,28 @@ int main(void)
 {
 	int n = 1024;
 	int a = 0;
-	int m, k, mi, sum1, sum2, sum, l;
+	int m, k, mi, sum1, sum2, sum;
 	sum1 = 0;
 	sum2 = 0;
 	mi = 0;
 	for (a=0; a <= (1024 / 3); a++)
 	{
 		m = (a * 3);
+		k = (a * 5);
 		if (m < n)
 		{
 			sum1 = (sum1 + m);
 		}
-	}
-	for (a = 0; a <= (1024 /5); a++)
-	{
+		printf(" c");
 		k = ( a * 5);
 		if (k < n)
 		{
 			sum2 = (sum2 + k);
 		}
-
-	}
-	for (a = 0; a< n; a++)
-	{
-		l = (a * 3);
-		if ((mi % 5) == 0)
+		printf("c");
+		if (k == m)
 		{
-			mi = (mi +l);
+			mi = (mi + m);
 		}
 	}
 	sum = (sum1 + sum2 - mi);
