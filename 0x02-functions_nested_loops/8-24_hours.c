9f2a4f51e7d971c00cfeb06e86761cf1c3f6312f
ita-ami <belkerfjaouad@gmail.com>
2023-06-21 07:05:42 -0700

    update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,19 +2,17 @@
 void jack_bauer(void)
 {
 	char a = '0';
+	char b = '0';
+	char c = '0';
+	char d = '0';
 
 	while (a <= '2')
 	{
-		char b = 'o';
 
 		while (b <= '3')
 		{
-			char c = '0';
-
 			while (c <= '5')
 			{
-				char d = '0';
-
 				while (d <= '9')
 				{
 					_putchar(a);
