58012e3ab238b17e49db42fe2d2e61bd56331e61
ita-ami <belkerfjaouad@gmail.com>
2023-06-23 11:51:29 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - the entry
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			if ((i % 5) == 0)
+			{
+				printf("FizzBuzz");
+			}
+			else
+				printf("Fizz");
+		}
+		else if ((i % 5) == 0)
+		{
+			if ((i % 3) == 0)
+			{
+				printf("FizzBuzz");
+			}
+			else
+				printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		printf("\n");
+	}
+	return (0);
+}
+
