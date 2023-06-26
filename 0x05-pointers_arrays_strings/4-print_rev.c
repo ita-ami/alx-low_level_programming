8fa21ba3a38b0db00d1238c27a96f6f9768dd7b2
ita-ami <belkerfjaouad@gmail.com>
2023-06-26 08:13:24 -0700

    update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,10 +3,12 @@
  *print_rev - a function that prints a string, in reverse
  *@s: the argument
  */
-void pri, k;
+void print_rev(char *s)
+{
 	char c, d;
+	int i, k;
 
-	for(i = 0; i < 1000; i++)
+	for (i = 0; i < 1000; i++)
 	{
 		c = s[i];
 		if (c == '\0')
@@ -15,7 +17,7 @@ void pri, k;
 			i = 1000;
 		}
 	}
-	for(i = (k - 1); i >= 0; i--)
+	for (i = (k - 1); i >= 0; i--)
 	{
 		d = s[i];
 		_putchar(d);
