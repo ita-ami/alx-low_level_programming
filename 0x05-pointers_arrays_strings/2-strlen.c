50b66aeef2c4a74328dd67b9a129c71fab635a33
ita-ami <belkerfjaouad@gmail.com>
2023-06-26 06:58:06 -0700

    create strlen file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_strlen - a function thet returns the length of a string
+ *@s: the argument
+ *Return: it's return the length of string
+ */
+int _strlen(char *s)
+{
+	int k, i;
+	char c;
+
+	for (i = 0; i <= 100; i++)
+	{
+		c = s[i];
+		if (c == '\0')
+		{
+			k = i;
+			i = 100;
+		}
+		i++;
+	}
+	return (k);
+}
