20c1c98cc28e3207ea3605767a4af917b360c1f0
ita-ami <belkerfjaouad@gmail.com>
2023-06-30 11:35:53 -0700

    update cap_string function file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,10 @@ char *cap_string(char *str)
 	for (i = 0; i < 1000; i++)
 	{
 		if ((str[i] == '\t') || (str[i] == '\t'))
-			str[i] = ' ';
+		{
+			if ((str[i + 1] >= 'a') && (str[i + 1] <= 'z'))
+				str[i + 1] = (str[i + 1] - 32);
+		}
 		else if (str[i] == '\0')
 			i = 1000;
 		else if (str[i] == ' ')
